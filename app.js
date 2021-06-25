c25ddd9d23965fe3da888eb502724c95b169cac9
adharsh-2024 <adharshr@brandeis.edu>
2021-06-25 00:05:32 -0400

    changed

--- app.js
+++ app.js
@@ -2,22 +2,75 @@
   app.js -- This creates an Express webserver
 */
 
-// First we load in all of the packages we need for the server...
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
+//added 942 june 23
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const layouts = require("express-ejs-layouts");
 const session = require("express-session");
-//const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const axios = require("axios");
 var debug = require("debug")("personalapp:server");
 
-// Now we create the server
+////////////////////////////////////////////
+//monguse stuff
+const mongoose = require( 'mongoose' );
+//mongoose.connect( `mongodb+srv://${auth.atlasAuth.username}:${auth.atlasAuth.password}@cluster0-yjamu.mongodb.net/authdemo?retryWrites=true&w=majority`);
+mongoose.connect( 'mongodb://localhost/personalapp'); //do we need to change this?
+//const mongoDB_URI = process.env.MONGODB_URI
+//mongoose.connect(mongoDB_URI)
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("we are connected!!!")
+});
+//////////////////////////////////////////
+const User = require('./models/User');
+const authRouter = require('./routes/authentication');
+const isLoggedIn = authRouter.isLoggedIn
+const loggingRouter = require('./routes/logging');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+
+
+
 const app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(cors());
+app.use(layouts);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(authRouter)
+app.use(loggingRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+const myLogger = (req,res,next) => {
+  console.log('inside a route!')
+  next()
+}
+// First we load in all of the packages we need for the server...
+
+
+
+
+
 // Here we specify that we will be using EJS as our view engine
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+
 
 // Here we process the requests so they are easy to handle
 app.use(express.json());
@@ -99,6 +152,191 @@ app.get("/c19",
       next(error)
     }
 })
+
+
+///app.get("/carboninterface",
+/*  async (req, res, next) => {
+    try {
+      const url = "https://www.carboninterface.com/api/v1/auth"
+      const result = await axios.get(url,
+      {  headers:
+        {
+          "Content-Type":"application/json",
+          "Authorization":"Bearer 6eDvnMFc8Ku2khxTR3A"
+        }
+      }
+      )
+      res.json(result.data)
+    } catch(error){
+      next(error)
+   }
+ }) */
+ app.get("/climateform", (request,response) => {
+   response.render("climatedataform")
+ })
+
+
+
+ app.post("/showmonthlyData",
+   async (req,res,next) => {
+     try {
+       const datafeature = req.body.datafeature
+       const datatype = req.body.datatype
+       const startyear = req.body.startyear
+       const endyear = req.body.endyear
+       const country = req.body.country
+       const url = "http://climatedataapi.worldbank.org/climateweb/rest/v1/country/"+datafeature+"/"+datatype+"/"+startyear+"/"+endyear+"/"+country
+       console.log(url)
+       const result = await axios.get(url)
+       console.dir(result.data)
+       console.log('results')
+       res.locals.results = result.data
+       res.render('showClimateData')
+     } catch(error){
+       next(error)
+     }
+ })
+
+
+
+
+
+app.get("/monthlyavgprec",
+  async (req, res, next) => {
+    try {
+      const url = "http://climatedataapi.worldbank.org/climateweb/rest/v1/country/mavg/pr/1940/1959/CHE"
+      const result = await axios.get(url)
+      console.dir(result.data)
+      console.log('results')
+      res.locals.results = result.data
+      res.render('showmonthlyavgprec')
+    } catch(error){
+      next(error)
+   }
+  })
+
+
+app.get("/monthlyavgtemp",
+  async (req, res, next) => {
+    try {
+      const url = "http://climatedataapi.worldbank.org/climateweb/rest/v1/country/mavg/tas/1940/1959/CHE"
+      const result = await axios.get(url)
+      console.dir(result.data)
+      console.log('results')
+      res.locals.results = result.data
+      res.render('showmonthlyavgtemp')
+    } catch(error){
+      next(error)
+   }
+  })
+
+app.get("/annualavgprec",
+  async (req, res, next) => {
+    try {
+      const url = "http://climatedataapi.worldbank.org/climateweb/rest/v1/country/annualavg/pr/1940/1959/CHE"
+      const result = await axios.get(url)
+      console.dir(result.data)
+      console.log('results')
+      res.locals.results = result.data
+      res.render('showannualavgprec')
+    } catch(error){
+      next(error)
+   }
+  })
+
+app.get("/annualavgtemp",
+  async (req, res, next) => {
+    try {
+      const url = "http://climatedataapi.worldbank.org/climateweb/rest/v1/country/annualavg/tas/1940/1959/CHE"
+      const result = await axios.get(url)
+      console.dir(result.data)
+      console.log('results')
+      res.locals.results = result.data
+      res.render('showannualavgtemp')
+    } catch(error){
+      next(error)
+   }
+  })
+
+app.get("/monthlyanomprec",
+  async (req, res, next) => {
+    try {
+      const url = "http://climatedataapi.worldbank.org/climateweb/rest/v1/country/manom/pr/2020/2039/CHE"
+      const result = await axios.get(url)
+      console.dir(result.data)
+      console.log('results')
+      res.locals.results = result.data
+      res.render('showmonthlyanomprec')
+    } catch(error){
+      next(error)
+   }
+  })
+
+app.get("/monthlyanomtemp",
+  async (req, res, next) => {
+    try {
+      const url = "http://climatedataapi.worldbank.org/climateweb/rest/v1/country/manom/tas/2020/2039/CHE"
+      const result = await axios.get(url)
+      console.dir(result.data)
+      console.log('results')
+      res.locals.results = result.data
+      res.render('showmonthlyanomtemp')
+    } catch(error){
+      next(error)
+   }
+  })
+
+app.get("/annualanomprec",
+  async (req, res, next) => {
+    try {
+      const url = "http://climatedataapi.worldbank.org/climateweb/rest/v1/country/annualanom/pr/2020/2039/CHE"
+      const result = await axios.get(url)
+      console.dir(result.data)
+      console.log('results')
+      res.locals.results = result.data
+      res.render('showannualanomprec')
+    } catch(error){
+      next(error)
+   }
+  })
+
+app.get("/annualanomtemp",
+  async (req, res, next) => {
+    try {
+      const url = "http://climatedataapi.worldbank.org/climateweb/rest/v1/country/annualanom/tas/2020/2039/CHE"
+      const result = await axios.get(url)
+      console.dir(result.data)
+      console.log('results')
+      res.locals.results = result.data
+      res.render('showannualanomtemp')
+    } catch(error){
+      next(error)
+   }
+  })
+
+
+  app.get("/apikey", async (req,res,next) => {
+    res.render('apikey')
+  })
+
+  const APIKey = require('./models/APIKey')
+
+  app.post("/apikey",
+    isLoggedIn,
+    async (req,res,next) => {
+      const domainName = req.body.domainName
+      const apikey = req.body.apikey
+      const apikeydoc = new APIKey({
+        userId:req.user._id,
+        domainName:domainName,
+        apikey:apikey
+      })
+      const result = await apikeydoc.save()
+      console.log('result=')
+      console.dir(result)
+      res.redirect('/apikeys')
+  })
+
 
 // this shows how to use an API to get recipes
 // http://www.recipepuppy.com/about/api/
